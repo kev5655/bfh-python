dcdae3b203dd7c6e479320460ee8bb478c2950c2
kevin <kevin@fidentity.ch>
2024-11-01 15:46:55 +0100

    move to different files

---
+++ solution/ex2/p14_int_to_bin_list.py
@@ -0,0 +1,12 @@
+from p13_int_to_bin import int_to_binary_bool_lst
+
+# 14
+def int_list_to_binary_bool_lst(lst: list[int]) -> list[list[bool]]:
+    result = []
+    for i in lst:
+        result.append(int_to_binary_bool_lst(i))
+    return result
+
+
+if __name__ == "main":
+    print(f'int list to binary list: {int_list_to_binary_bool_lst([1, 2, 3, 4, 5, 6, 7])}')
