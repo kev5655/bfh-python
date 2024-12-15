b03fd288089c547ae56a6e2ac08d29ca16e8b254
kevin <kevin@fidentity.ch>
2024-12-15 21:12:43 +0100

    finish p27

--- ex/p27_regex_on_file.py
+++ ex/p27_regex_on_file.py
@@ -1,18 +1,35 @@
 import re
 
-with open("./rsc/email-addresses.txt", "r") as f:
-    word_list = []
 
-    for line in f:
-        if line == "":
-            continue
-        word_list += re.split(r'[ \t]', line)
+def find_words():
+    with open("./rsc/email-addresses.txt", "r") as f:
+        word_list = []
 
-    regex = re.compile("[A-Z].*")
-    founds1 = []
-    for word in word_list:
-        if regex.match(word) and len(word) >= 2:
-            founds1 += [word]
+        for line in f:
+            if line == "":
+                continue
+            word_list += re.split(r'[ \t]', line)
 
-    for found in founds1:
-        print(found, end=", ")
+        regex = re.compile("[A-Z].*")
+        founds_words = []
+        for word in word_list:
+            if regex.match(word) and len(word) >= 2:
+                founds_words += [word]
+
+        for found in founds_words:
+            print(found, end=", ")
+
+        founds_emails = []
+        regex = re.compile(r"^\S+@\S+\.\S+$")
+        for word in word_list:
+            if regex.match(word):
+                founds_emails += [word]
+
+        print()
+
+        for found in founds_emails:
+            print(found, end=", ")
+
+
+if __name__ == "__main__":
+    find_words()
